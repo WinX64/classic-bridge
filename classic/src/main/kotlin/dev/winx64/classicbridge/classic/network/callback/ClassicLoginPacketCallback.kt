231834bb46883b2b71cc69ff8e22486cf1ca37f0
WinX64 <winx64b@hotmail.com>
2021-11-11 21:58:47 -0400

    Added Login and Play packet callbacks

---
+++ classic/src/main/kotlin/dev/winx64/classicbridge/classic/network/callback/ClassicLoginPacketCallback.kt
@@ -0,0 +1,17 @@
+package dev.winx64.classicbridge.classic.network.callback
+
+import dev.winx64.classicbridge.classic.network.clientbound.login.DisconnectPlayerClientboundLoginPacket
+import dev.winx64.classicbridge.classic.network.clientbound.login.ServerIdentificationClientboundLoginPacket
+import dev.winx64.classicbridge.classic.network.clientbound.login.cpe.ExtensionEntryClientboundLoginPacket
+import dev.winx64.classicbridge.classic.network.clientbound.login.cpe.ExtensionInfoClientboundLoginPacket
+
+interface ClassicLoginPacketCallback : ClassicPacketCallback {
+
+    fun onServerIdentification(packet: ServerIdentificationClientboundLoginPacket)
+
+    fun onDisconnectPlayer(packet: DisconnectPlayerClientboundLoginPacket)
+
+    fun onExtensionInfo(packet: ExtensionInfoClientboundLoginPacket)
+
+    fun onExtensionEntry(packet: ExtensionEntryClientboundLoginPacket)
+}
